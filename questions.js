9cc56a9cc786b905d977c662bc91a4907eed32bf
prathamsinghh <118873355+prathamsinghh@users.noreply.github.com>
2024-06-09 22:00:32 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,93 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What is the Capital of India",
+    answer: "Delhi",
+    options: ["Delhi", "Lucknow", "Indore"],
+  },
+  {
+    numb: 2,
+    question: "In which year India Got Independence?",
+    answer: "1947",
+    options: ["1950", "2020", "1947"],
+  },
+  {
+    numb: 3,
+    question: "Who is the Current PM of India?",
+    answer: "Narendra Modi",
+    options: ["Narendra Modi", "Rahul gandhi", "Arvind Kejriwal"],
+  },
+  {
+    numb: 4,
+    question: "What is the capital of U.P.?",
+    answer: "Lucknnow",
+    options: ["Lucknow", "Kanpur", "Agra"],
+  },
+  {
+    numb: 5,
+    question: "Who was the first PM of India?",
+    answer: "Jawahar lal Nehru",
+    options: ["Mahatma gandhi", "Jawahar lal Nehru", "Indra Gandhi"],
+  },
+  {
+    numb: 6,
+    question: "Among the 7 Wonders of world which is in India?",
+    answer: "Taj Mahal",
+    options: ["Great Wall of China", "Taj Mahal", "Giza Pyramid"],
+  },
+  {
+    numb: 7,
+    question: "Full form of WHO?",
+    answer: "World Health Organization",
+    options: ["World Health Orientation", "Why Health Organization", "World Health Organization"],
+  },
+  {
+    numb: 8,
+    question: "Who is the CM of UP?",
+    answer: "Yogi Adityanath",
+    options: ["Rahul Gandhi", "Yogi Adityanath", "Akhilesh Yadav"],
+  },
+  {
+    numb: 9,
+    question: "Capital of Madhya Pradesh?",
+    answer: "Indore",
+    options: ["Indore", "Bhopal", "kashmir"],
+  },
+  {
+    numb: 10,
+    question: "Presiden of India?",
+    answer: "Draupdi Murmu",
+    options: ["RamNath Kovind", "Draupdi Murmu", "Rahul Gandhi"],
+  },
+  {
+    numb: 11,
+    question: "Who is the owner of PSIT?",
+    answer: "Pranveer Singh",
+    options: ["Pranveer Singh", "Prashant Singh", "Rajesh Pandey"],
+  },
+  {
+    numb: 12,
+    question: "In which City PSIT is located?",
+    answer: "Kanpur",
+    options: ["Kanpur", "Lucknow", "Mathura"],
+  },
+  {
+    numb: 13,
+    question: "What is the full form of ISRO?",
+    answer: "India space Resarch Organisation",
+    options: ["India space Resarch Organisation", "India space Remote Organisation", "India system Resarch Organisation"],
+  },
+  {
+    numb: 14,
+    question: "What is the full form of IP?",
+    answer: "Internet Protocol",
+    options: ["Internet Protocol", "Internet Price", "International Price"],
+  },
+  {
+    numb: 15,
+    question: "What is the full form of ERP?",
+    answer: "Enterprise Resource Locator",
+    options: ["Enterprise Resarch Locator", "Enterprise Resource Locator", "Enterprise Reource local"],
+  },
+];
